a5340fe298a338bc98d6f24810ca705939cc01de
Igor <Igor.ivanushenkov@yandex.ru>
2020-01-26 17:05:33 +0300

    Добавили возможность постить кнопкой (+)

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
+
+
 
 # Create your views here.
 
@@ -11,9 +14,35 @@ def post_list(request):
     :param request:
     :return:
     """
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
+    """
+    Стриница с отдельным постом
+    :param request:
+    :param pk:
+    :return:
+    """
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    """
+    Добавляем новый пост кнопкой "+"
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
